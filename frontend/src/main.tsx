079e2c960ddce17764ef4fbfbc1b270afbd586ff
Yusufolosun <olosun.m1904582@st.futminna.edu.ng>
2026-02-28 04:00:59 +0100

    feat(frontend): wire ToastProvider and ToastContainer into root

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { WalletProvider } from './context/WalletContext.tsx'
+import { ToastProvider } from './context/ToastContext.tsx'
+import { ToastContainer } from './components/ToastContainer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <WalletProvider>
-      <App />
+      <ToastProvider>
+        <App />
+        <ToastContainer />
+      </ToastProvider>
     </WalletProvider>
   </StrictMode>,
 )
